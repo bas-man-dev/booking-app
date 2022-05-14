6c2a73fd426a7827682881e99bcfa92e6b90b019
Bas Man <bas.man.developer@gmail.com>
2022-05-14 14:54:30 +0100

    Welcome message using format print

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World!")
+
+	var conferenceName = "Fantastic Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("\nWelcome to the %s booking application:\n", conferenceName)
+	fmt.Printf("We have %d tickets and %d available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Continue here to get your tickets to attend the conference")
+
 }
