481d6bc5abcec52840f5f77d2530263d37993724
Bas Man <bas.man.developer@gmail.com>
2022-05-15 18:00:32 +0100

    made functions and global variables to clean code.

--- main.go
+++ main.go
@@ -6,68 +6,36 @@ import (
 	"time"
 )
 
+const conferenceTickets uint = 50
+
+var conferenceName = "Fantastic Conference"
+var remainingTickets uint = 50
+var bookings = []string{}
+
 func main() {
 
-	conferenceName := "Fantastic Conference"
-	const conferenceTickets int = 50
-	var remainingTickets uint = 50
-	bookings := []string{}
-
-	fmt.Printf("\nWelcome to the %v booking application:\n", conferenceName)
-	fmt.Printf("We have %v tickets and %v available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Continue here to get your tickets to attend the conference")
+	greetUser()
 
 	for {
+		// get user details
 
-		firstName := ""
-		lastName := ""
-		email := ""
-		var userTickets uint
+		firstName, lastName, email, userTickets := getUserDetails()
 
-		fmt.Println("Please enter your first name: ")
-		fmt.Scan(&firstName)
+		// Validate user details
 
-		fmt.Println("Please enter your last name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Please enter your email address: ")
-		fmt.Scan(&email)
-
-		fmt.Printf("%v Tickets left. How many tickets would you like? \n", remainingTickets)
-		fmt.Scan(&userTickets)
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
-
-		if !isValidName {
-			fmt.Println("First name and Last name need to be 2 or more characters long")
+		detailsCheck := userDetailsValidation(firstName, lastName, email, userTickets)
+		if !detailsCheck {
 			continue
 		}
-		if !isValidEmail {
-			fmt.Println("email address needs an '@' sign")
-			continue
-		}
-		if !isValidTickets {
-			fmt.Println("Ticket number was invalid.")
-			continue
-		}
-		now := time.Now()
 
-		remainingTickets -= userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		// Do the transaction:
 
-		fmt.Printf("Thank you for the purchase %v %v.\nYou bought %v tickets.\nYou will receive a confirmation email to:\n%v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("Purchased at: %v:%v on the %v %v %v \n", now.Hour(), now.Minute(), now.Day(), now.Month(), now.Year())
+		updateBookings(userTickets, firstName, lastName, email)
 
-		fmt.Println(remainingTickets)
+		fmt.Printf("Remaining tickets: %v \n", remainingTickets)
 
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("All user bookings: %v\n", firstNames)
+		firstNames := getFirstNames()
+		fmt.Printf("The first names of bookings clients are: %v \n", firstNames)
 
 		if remainingTickets == 0 {
 			fmt.Printf("%v fully booked... See you again next year!\n", conferenceName)
@@ -75,5 +43,75 @@ func main() {
 		}
 
 	}
+
+}
+
+func greetUser() {
+
+	fmt.Printf("\nWelcome to the %v booking application:\n", conferenceName)
+	fmt.Printf("We have %v tickets and %v available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Continue here to get your tickets to attend the conference")
+}
+
+func getFirstNames() []string {
+
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func getUserDetails() (string, string, string, uint) {
+	firstName := ""
+	lastName := ""
+	email := ""
+	var userTickets uint
+
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Printf("%v Tickets left. How many tickets would you like? \n", remainingTickets)
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func userDetailsValidation(firstName string, lastName string, email string, userTickets uint) bool {
+
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTickets := userTickets > 0 && userTickets <= remainingTickets
+
+	if !isValidName {
+		fmt.Println("First name and Last name need to be 2 or more characters long")
+		return false
+	}
+	if !isValidEmail {
+		fmt.Println("email address needs an '@' sign")
+		return false
+	}
+	if !isValidTickets {
+		fmt.Println("Ticket number was invalid.")
+		return false
+	}
+	return true
+}
+
+func updateBookings(userTickets uint, firstName string, lastName string, email string) {
+	now := time.Now()
+
+	remainingTickets -= userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you for the purchase %v %v.\nYou bought %v tickets.\nYou will receive a confirmation email to:\n%v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("Purchased at: %v:%v on the %v %v %v \n", now.Hour(), now.Minute(), now.Day(), now.Month(), now.Year())
 
 }
